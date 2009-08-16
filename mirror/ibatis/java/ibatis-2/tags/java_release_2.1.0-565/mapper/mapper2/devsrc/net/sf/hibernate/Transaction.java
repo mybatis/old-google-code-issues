e46d2dc56f524c1aaf9f2b43600c338a6f1204c2
cbegin <cbegin@6a4a4fef-ff2a-56b0-fee9-946765563393>
2009-08-16 05:09:20 +0000

    Moved remotely

---
+++ mirror/ibatis/java/ibatis-2/tags/java_release_2.1.0-565/mapper/mapper2/devsrc/net/sf/hibernate/Transaction.java
@@ -0,0 +1,9 @@
+package net.sf.hibernate;
+
+public class Transaction {
+    public void commit() {
+    }
+
+    public void rollback() {
+    }
+}
---
+++ mirror/ibatis/java/ibatis-2/tags/java_release_2.1.0-565/mapper/mapper2/devsrc/net/sf/hibernate/cfg/Configuration.java
@@ -0,0 +1,17 @@
+package net.sf.hibernate.cfg;
+
+import net.sf.hibernate.SessionFactory;
+
+import java.util.Properties;
+
+public class Configuration {
+    public void addClass(Class aClass) {
+    }
+
+    public void setProperties(Properties props) {
+    }
+
+    public SessionFactory buildSessionFactory() {
+        return null;
+    }
+}
