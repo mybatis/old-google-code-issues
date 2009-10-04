372c5ea4acd66775a8e81ca997014e0ef6234223
cbegin <cbegin@6a4a4fef-ff2a-56b0-fee9-946765563393>
2009-10-04 05:29:40 +0000

    Fixed IBATIS-660 SQL parser does not handle #{...} parsing correctly if a "}" char presides the first tag in the SQL statement

--- mirror/ibatis/java/ibatis-3/trunk/ibatis-3-core/src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
+++ mirror/ibatis/java/ibatis-3/trunk/ibatis-3-core/src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
@@ -20,19 +20,26 @@ public class GenericTokenParser {
     StringBuilder builder = new StringBuilder();
     if (text != null) {
       String after = text;
-
       int start = after.indexOf(openToken);
       int end = after.indexOf(closeToken);
-      while (start > -1 && end > start) {
-        String before = after.substring(0, start);
-        String content = after.substring(start + openToken.length(), end);
-        String substitution = handler.handleToken(content);
-        builder.append(before);
-        builder.append(substitution);
-        after = after.substring(end + closeToken.length());
+      while (start > -1) {
+        if (end > start) {
+          String before = after.substring(0, start);
+          String content = after.substring(start + openToken.length(), end);
+          String substitution = handler.handleToken(content);
+          builder.append(before);
+          builder.append(substitution);
+          after = after.substring(end + closeToken.length());
+        } else if (end > -1) {
+          String before = after.substring(0, end);
+          builder.append(before);
+          builder.append(closeToken);
+          after = after.substring(end + closeToken.length());
+        } else {
+          break;
+        }
         start = after.indexOf(openToken);
         end = after.indexOf(closeToken);
-
       }
       builder.append(after);
     }
