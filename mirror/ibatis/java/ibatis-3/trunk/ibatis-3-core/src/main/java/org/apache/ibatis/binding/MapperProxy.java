e1bde4fc5b4339ed81684be6cd5828a627e4d091
cbegin <cbegin@6a4a4fef-ff2a-56b0-fee9-946765563393>
2010-01-20 03:33:22 +0000

    fixed NPE on null primitives

--- mirror/ibatis/java/ibatis-3/trunk/ibatis-3-core/src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ mirror/ibatis/java/ibatis-3/trunk/ibatis-3-core/src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -5,17 +5,42 @@ import org.apache.ibatis.session.SqlSession;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapperProxy implements InvocationHandler {
+
+  private static final Set<String> OBJECT_METHODS = new HashSet<String>() {{
+    add("toString");
+    add("getClass");
+    add("hashCode");
+    add("equals");
+    add("wait");
+    add("notify");
+    add("notifyAll");
+  }};
 
   private SqlSession sqlSession;
 
   private <T> MapperProxy(SqlSession sqlSession) {
     this.sqlSession = sqlSession;
   }
-
+  
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    return new MapperMethod(method, sqlSession).execute(args);
+    try {
+      if (!OBJECT_METHODS.contains(method.getName())) {
+        final MapperMethod mapperMethod = new MapperMethod(method, sqlSession);
+        final Object result = mapperMethod.execute(args);
+        if (result == null && method.getReturnType().isPrimitive()) {
+          throw new BindingException("Mapper method '"+ method.getName()+"' ("+method.getDeclaringClass()+") attempted to return null from a method with a primitive return type ("+method.getReturnType()+").");
+        }
+        return result;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
   }
 
   public static <T> T newMapperProxy(Class<T> mapperInterface, SqlSession sqlSession) {
