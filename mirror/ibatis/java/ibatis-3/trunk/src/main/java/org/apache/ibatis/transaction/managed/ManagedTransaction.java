c5b52beef92bc2ddc943053da627e8561207443c
cbegin <cbegin@6a4a4fef-ff2a-56b0-fee9-946765563393>
2010-04-21 02:46:22 +0000

    closed connection in managed transaction

--- mirror/ibatis/java/ibatis-3/trunk/src/main/java/org/apache/ibatis/transaction/managed/ManagedTransaction.java
+++ mirror/ibatis/java/ibatis-3/trunk/src/main/java/org/apache/ibatis/transaction/managed/ManagedTransaction.java
@@ -1,11 +1,15 @@
 package org.apache.ibatis.transaction.managed;
 
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.transaction.Transaction;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ManagedTransaction implements Transaction {
+
+  private Log log = LogFactory.getLog(ManagedTransaction.class);
 
   private Connection connection;
 
@@ -26,7 +30,12 @@ public class ManagedTransaction implements Transaction {
   }
 
   public void close() throws SQLException {
-    // Does nothing
+    try {
+      if (connection != null) connection.close();
+    } catch (SQLException e) {
+      // Log and ignore.  Nothing more that should be done here.
+      log.error(e.getMessage(), e);
+    }
   }
 
 }
