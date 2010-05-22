985365615d6d65d2d4754338806f4ed94c1fb6d4
clinton.begin <clinton.begin@6a4a4fef-ff2a-56b0-fee9-946765563393>
2010-05-22 01:18:33 +0000

    mavenized project

--- branches/ibatis-2-maintenance/ibatis-2-core/build/setenv.bat
+++
@@ -1,7 +0,0 @@
-echo Using %JAVA_HOME%
-
-set BUILD_CP=%CLASSPATH%
-set BUILD_CP=%BUILD_CP%;%JAVA_HOME%\lib\tools.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\ant.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\optional.jar;
-set BUILD_CP=%BUILD_CP%;..\devlib\junit.jar;
