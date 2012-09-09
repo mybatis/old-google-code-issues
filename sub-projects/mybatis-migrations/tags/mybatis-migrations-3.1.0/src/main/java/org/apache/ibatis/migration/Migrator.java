c079854eb163be49582a15f76baec70a3523e05a
clinton.begin@gmail.com <clinton.begin@gmail.com@6a4a4fef-ff2a-56b0-fee9-946765563393>
2012-09-09 18:52:57 +0000

    Release tag for 3.1.0

---
+++ sub-projects/mybatis-migrations/tags/mybatis-migrations-3.1.0/src/main/java/org/apache/ibatis/migration/Migrator.java
@@ -0,0 +1,7 @@
+package org.apache.ibatis.migration;
+
+public class Migrator {
+  public static void main(String[] args) {
+    new CommandLine(args).execute();
+  }
+}
---
+++ sub-projects/mybatis-migrations/tags/mybatis-migrations-3.1.0/src/main/java/org/apache/ibatis/migration/options/Options.java
@@ -0,0 +1,13 @@
+package org.apache.ibatis.migration.options;
+
+public enum Options {
+  PATH,
+  ENVPATH,
+  SCRIPTPATH,
+  DRIVERPATH,
+  ENV,
+  FORCE,
+  TRACE,
+  HELP,
+  TEMPLATE
+}
