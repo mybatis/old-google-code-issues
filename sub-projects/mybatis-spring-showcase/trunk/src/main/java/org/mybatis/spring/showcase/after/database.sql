b7370a6b4b515b4b01013678c980a9a99287cc76
eduardo.macarron <eduardo.macarron@6a4a4fef-ff2a-56b0-fee9-946765563393>
2012-11-17 18:15:44 +0000

    Showcase

---
+++ sub-projects/mybatis-spring-showcase/trunk/src/main/java/org/mybatis/spring/showcase/after/database.sql
@@ -0,0 +1,10 @@
+create table blog (
+    id int not null,
+    name varchar(80) not null,
+    constraint pk_user primary key (id)
+);
+
+insert into blog VALUES (1, 'Pocoyo');
+insert into blog VALUES (2, 'Pato');
+insert into blog VALUES (3, 'Eli');
+insert into blog VALUES (4, 'Valentina');
